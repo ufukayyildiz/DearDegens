dd52305807e33743add77fd21798270b03408ffa
DollaHane <shanehubsch@gmail.com>
2023-09-25 14:04:10 +0200

    continued build

--- src/app/mymints/page.tsx
+++ src/app/mymints/page.tsx
@@ -1,15 +1,30 @@
 import React from "react"
 import { db } from "@/src/db"
-import MyMintsFeed from "@/src/components/pageMyMint/MyMintsFeed"
+import MyMintsComponent from "@/src/components/pageMyMint/MyMintsComponent"
 import { listingsGeneral } from "@/src/db/schema"
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/src/lib/auth/auth-options"
+import { eq } from "drizzle-orm"
+
 
 export default async function MyMints() {
-  const listings = await db.select().from(listingsGeneral)
-  console.log('Listings:', listings)
+  
+  const session = await getServerSession(authOptions)
+  const userId = JSON.stringify(session?.user.id)
+  const listings = await db.select().from(listingsGeneral).where(eq(listingsGeneral.authorId, userId))
 
   return (
-    <div className="w-full">
-      <MyMintsFeed generalListing={listings} />
+    <div className="z-20 mx-auto w-11/12 min-w-[280px] overflow-hidden md:w-9/12">
+      <ul className="mx-5 mb-44 mt-36 flex h-full flex-col space-y-8">
+        {listings.map((listing) => {
+          return (
+            <MyMintsComponent
+              key={listing.id}
+              listing={listing}
+            />
+          )
+        })}
+      </ul>
     </div>
   )
 }
--- src/components/pageMyMint/MyMintsComponent.tsx
+++ src/components/pageMyMint/MyMintsComponent.tsx
@@ -1,74 +1,83 @@
 "use client"
-import { useState, useEffect } from "react"
+
+import { useEffect, useState } from "react"
 import Link from "next/link"
 import { formatTimeToNow } from "@/src/lib/utils"
 import { GeneralListing } from "@/src/types/db"
 
 interface GeneralListingProps {
-  generalListing: GeneralListing
+  listing: GeneralListing
 }
 
-export default function MyMintsComponent({ generalListing}: GeneralListingProps) {
-
+export default function MyMintsComponent({
+  listing,
+}: GeneralListingProps) {
   const [adImage, setAdImage] = useState([])
-  const jsonImage = generalListing.images
-  
+  const jsonImage = listing.images
+
   useEffect(() => {
     if (jsonImage) {
-      const images = JSON.parse(jsonImage);
-      console.log('images:', images);
-      setAdImage(images);
+      const images = JSON.parse(jsonImage)
+      console.log("images:", images)
+      setAdImage(images)
     }
   }, [jsonImage])
 
-
   return (
-    <div className="mx-auto w-full max-w-[800px] rounded-lg border border-l-4 border-secondary border-l-teal-400 bg-background p-3 shadow-md">
-      <div className="relative flex justify-between">
-
-        {/* INFO */}
-        <div className="relative w-8/12">
-          
-          <div>
-            <a href={`/fs/post/${generalListing.id}`}>
-              <h1 className="text-primary group-hover:text-teal-400 md:text-xl mb-2">
-                {generalListing.title}
+    <div className="mx-auto w-full max-w-[800px] rounded-lg border border-l-4 border-secondary border-l-teal-400 bg-background shadow-md transition duration-500 hover:scale-95">
+      <Link href={`/fs/post/${listing.id}`}>
+        <div className="relative h-48 flex justify-between ">
+          {/* INFO */}
+          <div className="relative w-8/12 p-3">
+            <div>
+              <h1 className="text-primary font-bold group-hover:text-teal-400 md:text-xl mb-2">
+                {listing.title}
               </h1>
-            </a>
+            </div>
+
+            <div className="relative max-h-20 overflow-hidden w-full mb-5 text-clip text-zinc-500">
+              <p>{listing.description}</p>
+            </div>
+
+            <div className="absolute bottom-6">
+              <h1 className="text-primary group-hover:text-teal-400 font-semibold text-lg">
+                R {listing.price}
+              </h1>
+            </div>
+
+            <div className="absolute bottom-2 left-3 flex max-h-40 gap-1 text-xs italic text-capecod-500">
+              <span>Listed</span>
+              {formatTimeToNow(new Date(listing.createdAt))}
+              <span>in</span>
+              <span className="text-red-400 font-bold">
+                {listing.location}
+              </span>
+            </div>
           </div>
 
-          <div className="relative max-h-20 overflow-hidden w-full mb-5 text-clip text-xs text-zinc-500">
-            <p>{generalListing.description}</p>
+          {/* IMAGE */}
+          <div className="h-full w-4/12">
+            <img
+              src={adImage[0]}
+              alt={adImage[0]}
+              className="h-full w-full object-cover rounded-lg"
+            />
           </div>
 
-            <h1 className="text-primary group-hover:text-teal-400 font-semibold text-lg">
-              R {generalListing.price}
-            </h1>
-          <div className="absolute bottom-0 left-0 flex max-h-40 gap-1 text-xs italic text-capecod-500">
-            <span>Listed</span>
-            {formatTimeToNow(new Date(generalListing.createdAt))}
-            <span>in</span>
-            <span className="text-red-400 font-bold">{generalListing.location}</span>
+          {/* TAGS */}
+          <div className=" absolute -bottom-3 -right-3">
+            {listing.isAvailable === true ? (
+              <div className="h-6 w-20 text-xs text-center justify-center italic bg-teal-400 rounded-full p-1 shadow-lg z-30">
+                Available!
+              </div>
+            ) : (
+              <div className="h-6 w-20 text-xs text-center justify-center italic bg-amber-300 rounded-full p-1 shadow-lg z-30">
+                Sold
+              </div>
+            )}
           </div>
         </div>
-
-        {/* IMAGE */}
-        <img src={adImage[0]} alt={adImage[0]} className=" w-36 h-36 object-cover rounded-lg"/>
-
-        {/* TAGS */}
-        <div className=" absolute -bottom-5 -right-5">
-          {generalListing.isAvailable === true ? (
-            <div className="h-6 w-20 text-xs text-center justify-center italic bg-cyan-300 rounded-full p-1 shadow-lg z-30">
-              Available!
-            </div>
-          ) : (
-            <div className="h-6 w-20 text-xs text-center justify-center italic bg-amber-300 rounded-full p-1 shadow-lg z-30">
-              Sold
-            </div>
-          )}
-        </div>
-
-      </div>
+      </Link>
     </div>
   )
 }
