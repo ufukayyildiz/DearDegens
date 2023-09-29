4592494c23c5f4a2dc0c3eb89997a99b6f7230a5
DollaHane <shanehubsch@gmail.com>
2023-09-29 23:09:26 +0200

    continued build

--- src/app/p/mymints/page.tsx
+++ src/app/p/mymints/page.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import { db } from "@/src/db"
 import MyMintsComponent from "@/src/components/pageMyMint/MyMintsComponent"
-import { Separator } from "@/src/components/components-ui/Separator"
 import { listingsGeneral, listingsProperty } from "@/src/db/schema"
 import { getServerSession } from "next-auth"
 import { authOptions } from "@/src/lib/auth/auth-options"
@@ -15,10 +14,10 @@ export default async function MyMints() {
   const household = await db.select().from(listingsGeneral).where(eq(listingsGeneral.authorId, userId))
   const property = await db.select().from(listingsProperty).where(eq(listingsProperty.authorId, userId))
 
-  const userListings = [{
-    household,
-    property
-  }]
+  const listings = [...household, ...property]
+  listings.sort((a:any, b: any) => b.createdAt - a.createdAt);
+
+  
 
   return (
     <div className="z-20 mx-auto w-11/12 min-w-[280px] overflow-hidden md:w-8/12">
@@ -27,7 +26,7 @@ export default async function MyMints() {
       </h1>
       <hr className="my-2 border border-t-muted-foreground"/>
       <ul className="mb-44 mt-10 flex w-full h-full flex-col space-y-8 px-5">
-        {userListings.map((listing) => {
+        {listings.map((listing) => {
           return (
             <MyMintsComponent
               key={listing.id}
