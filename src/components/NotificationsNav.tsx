605e01f1c8b1a894195941a7dc69a6675fba19ac
DollaHane <shanehubsch@gmail.com>
2023-10-23 11:44:28 +0200

    Continued build

--- src/components/NotificationsNav.tsx
+++ src/components/NotificationsNav.tsx
@@ -2,12 +2,14 @@
 
 import { useEffect, useState } from "react"
 import Link from "next/link"
+import { useRouter } from "next/navigation"
 import axios from "axios"
-import { Bell } from "lucide-react"
+import { Bell, MoreVertical } from "lucide-react"
 
 import { formatTimeToNow } from "../lib/utils"
 import { notificationsType } from "../types/db"
 import { setUnReadNotifications } from "./components-global/store"
+import { Button } from "./components-ui/Button"
 import {
   Dialog,
   DialogContent,
@@ -24,7 +26,6 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./components-ui/DropdownMenu"
-import { useRouter } from "next/navigation"
 
 interface NotificationsNavProps {
   userNotifications: notificationsType
@@ -39,17 +40,14 @@ type Notication = {
 }
 
 export function NotificationsNav({ userNotifications }: NotificationsNavProps) {
-
   const notifications = [...userNotifications]
-      notifications.sort((a: any, b: any) => b.createdAt - a.createdAt)
+  notifications.sort((a: any, b: any) => b.createdAt - a.createdAt)
   const router = useRouter()
   const [unreadNotifications, setUnreadNotifications] = useState<number>()
   const [selectedNotificationId, setSelectedNotificationId] =
     useState<string>("")
   const [displayedNotification, setDisplayedNotification] =
     useState<Notication | null>()
-
-  
 
   const handleNotificationSelected = async (notify: any) => {
     setSelectedNotificationId(notify.id)
@@ -80,7 +78,6 @@ export function NotificationsNav({ userNotifications }: NotificationsNavProps) {
     getUnreadArray()
   }, [selectedNotificationId, notifications])
 
-
   useEffect(() => {
     if (notifications) {
       const selectedNotification = notifications.find(
@@ -99,7 +96,7 @@ export function NotificationsNav({ userNotifications }: NotificationsNavProps) {
           <DropdownMenuTrigger>
             <div className="relative">
               <Bell className="w-6 h-6" />
-              {unreadNotifications !== undefined &&unreadNotifications > 0 && (
+              {unreadNotifications !== undefined && unreadNotifications > 0 && (
                 <div className="absolute flex -top-3 -right-3 w-6 h-6 bg-red-500 content-center rounded-full shadow-md">
                   <p className="absolute top-1 w-full mx-auto text-white text-xs text-center">
                     {unreadNotifications}
@@ -113,41 +110,87 @@ export function NotificationsNav({ userNotifications }: NotificationsNavProps) {
             className="relative max-h-[350px] overflow-y-auto"
             align="end"
           >
-            <h1 className="font-bold">Notifications</h1>
-            {notifications && notifications.map((notify: any, index: number) => (
-              <div key={index} className="relative">
-                <DropdownMenuSeparator />
-                <DialogTrigger
-                  onClick={() => handleNotificationSelected(notify)}
-                >
+            <h1 className="font-bold text-2xl">Notifications</h1>
+            <div className="w-full flex py-2 justify-between">
+              <Button variant="outline">Mark all as read</Button>
+              <Button variant="outline">Delete all</Button>
+            </div>
+            {notifications &&
+              notifications.map((notify: any, index: number) => (
+                <div key={index} className="relative">
+                  <DropdownMenuSeparator />
+
                   {notify.isRead === false ? (
                     <DropdownMenuItem asChild className="text-start">
                       <div className="grid grid-cols-1 w-72 content-start border-teal-500 border-2 bg-teal-300/10">
-                        <h1 className="font-semibold truncate">
-                          {notify.title}
-                        </h1>
-                        <div className="flex max-h-40 gap-1 text-xs italic text-secondary">
-                          <span>Created</span>
-                          {formatTimeToNow(new Date(notify.createdAt))}
-                        </div>
+                        <DialogTrigger
+                          onClick={() => handleNotificationSelected(notify)}
+                        >
+                          <h1 className="text-start font-semibold truncate">
+                            {notify.title}
+                          </h1>
+                          <div className="flex max-h-40 gap-1 text-xs italic text-secondary">
+                            <span>Created</span>
+                            {formatTimeToNow(new Date(notify.createdAt))}
+                          </div>
+                        </DialogTrigger>
+                        <DropdownMenu>
+                          <DropdownMenuTrigger>
+                            <Button className="w-10 h-10 bg-transparent group hover:bg-white border border-transparent rounded-full shadow-none hover:shadow-md absolute top-1 right-1">
+                              <MoreVertical className="w-6 h-6 mx-auto text-transparent group-hover:text-zinc-800 rounded-full absolute top-2" />
+                            </Button>
+                          </DropdownMenuTrigger>
+                          <DropdownMenuContent
+                            className="relative max-h-[350px] overflow-y-auto"
+                            align="end"
+                          >
+                            <DropdownMenuItem asChild className="text-start">
+                              <p>Delete</p>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem asChild className="text-start">
+                              <p>Mark as read</p>
+                            </DropdownMenuItem>
+                          </DropdownMenuContent>
+                        </DropdownMenu>
                       </div>
                     </DropdownMenuItem>
                   ) : (
                     <DropdownMenuItem asChild className="text-start">
-                      <div className="grid grid-cols-1 w-72 content-start">
-                        <h1 className="font-semibold truncate">
-                          {notify.title}
-                        </h1>
-                        <div className="flex max-h-40 gap-1 text-xs italic text-secondary">
-                          <span>Created</span>
-                          {formatTimeToNow(new Date(notify.createdAt))}
-                        </div>
+                      <div className="relative grid grid-cols-1 w-72 content-start">
+                        <DialogTrigger
+                          onClick={() => handleNotificationSelected(notify)}
+                        >
+                          <h1 className="text-start font-semibold truncate">
+                            {notify.title}
+                          </h1>
+                          <div className="flex max-h-40 gap-1 text-xs italic text-secondary">
+                            <span>Created</span>
+                            {formatTimeToNow(new Date(notify.createdAt))}
+                          </div>
+                        </DialogTrigger>
+                        <DropdownMenu>
+                          <DropdownMenuTrigger>
+                            <Button className="w-10 h-10 bg-transparent group hover:bg-white border border-transparent rounded-full shadow-none hover:shadow-md absolute top-1 right-1">
+                              <MoreVertical className="w-6 h-6 mx-auto text-transparent group-hover:text-zinc-800 rounded-full absolute top-2" />
+                            </Button>
+                          </DropdownMenuTrigger>
+                          <DropdownMenuContent
+                            className="relative max-h-[350px] overflow-y-auto"
+                            align="end"
+                          >
+                            <DropdownMenuItem asChild className="text-start">
+                              <p>Delete</p>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem asChild className="text-start">
+                              <p>Mark as read</p>
+                            </DropdownMenuItem>
+                          </DropdownMenuContent>
+                        </DropdownMenu>
                       </div>
                     </DropdownMenuItem>
                   )}
-                </DialogTrigger>
-              </div>
-            ))}
+                </div>
+              ))}
           </DropdownMenuContent>
         </DropdownMenu>
 
