9b69c5722e0b6a9a95db0bec33e9095799297c3e
DollaHane <shanehubsch@gmail.com>
2024-02-21 15:25:47 +0200

    connecting server

--- src/components/pageMintChat/ChatSheet.tsx
+++ src/components/pageMintChat/ChatSheet.tsx
@@ -14,7 +14,7 @@ import {
 } from "../components-ui/Sheet"
 
 import { io, Socket } from "socket.io-client"
-const socket: Socket = io("http://localhost:4000")
+const socket: Socket = io("https://dear-degens-server.vercel.app")
 
 export default function ChatSheet(ListingId: any) {
 
