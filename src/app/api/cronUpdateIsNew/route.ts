ec2cc30787f2faa8a3a5663c1534403ddbc3c963
DollaHane <shanehubsch@gmail.com>
2024-04-15 22:02:46 +0200

    fixed cron jobs code

--- src/app/api/cronUpdateIsNew/route.ts
+++ src/app/api/cronUpdateIsNew/route.ts
@@ -1,7 +1,7 @@
 import { db } from "@/src/server/db"
 import { sql } from "drizzle-orm"
 
-export async function updateIsNew() {
+export async function PATCH() {
   try {
     await db.execute(
       sql.raw(
@@ -13,8 +13,10 @@ export async function updateIsNew() {
       `
       )
     )
-    console.log("Successfully updated isNew tags.")
+    
+    return new Response("Successfully updated isNew tags.", { status: 200 })
   } catch (error) {
     console.error("Failed to update isNew tags.", error)
+    return new Response("Failed to update isNew tags.", { status: 500 })
   }
 }
