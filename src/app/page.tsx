f3676b3dac957342a8cc0a77866bc1dcb4de70b6
DollaHane <shanehubsch@gmail.com>
2024-05-04 22:53:38 +0200

    Removed Unused '@ts-expect-error' directive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,6 @@ export default async function HomePage() {
       <div className="z-20 mx-auto h-auto w-10/12 min-w-[280px] md:w-8/12">
         <h1 className="mt-10 text-xl font-bold text-primary">Recently Added</h1>
         <hr className="my-2 border border-t-muted-foreground" />
-
-        {/* @ts-expect-error Server Component  */}
         <HomeCarousel />
       </div>
     </section>
