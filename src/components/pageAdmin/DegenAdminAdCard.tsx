4934069f2637bc87981c4cc3d607cfbbf7d716b1
DollaHane <shanehubsch@gmail.com>
2024-05-30 14:06:23 +0200

    Added {/* @ts-expect-error Server Component */}

--- src/components/pageAdmin/DegenAdminAdCard.tsx
+++ src/components/pageAdmin/DegenAdminAdCard.tsx
@@ -10,6 +10,7 @@ interface DegenAdminAdCardProps {
 export default function DegenAdminAdCard({ listing }: DegenAdminAdCardProps) {
   return (
     <div className="relative mx-auto w-11/12">
+      {/* @ts-expect-error Server Component */}
       <DegenAdminAdCardActions listing={listing} />
       <div className="relative flex flex-row">
         <div className="h-40 w-40">
