d9f898e1de75eb9d04f44dcb51dec5afbb3cfa2a
DollaHane <shanehubsch@gmail.com>
2024-06-09 13:39:33 +0200

    Updated banner responsiveness

--- src/components/pageHome/Banner.tsx
+++ src/components/pageHome/Banner.tsx
@@ -8,25 +8,25 @@ export default function Banner() {
   const { data: session } = useSession()
 
   return (
-    <div className="group relative mb-10 flex h-40 w-full items-center justify-center overflow-hidden rounded-2xl object-cover shadow-lg">
+    <div className="group relative mb-10 flex w-full items-center justify-center overflow-hidden rounded-2xl border-none object-cover shadow-lg">
       <Image
         src={Splash}
         alt="deardegens"
         className="w-full object-cover transition duration-200 group-hover:scale-[1.1]"
       />
 
-      <div className="h-8/12 absolute flex w-11/12 flex-col items-center justify-center rounded-lg bg-background/70 p-8 shadow-lg backdrop-blur-sm">
+      <div className="absolute flex h-[100vh] w-[100vw] flex-col items-center justify-center rounded-lg border-none bg-background/50 p-8 text-center shadow-lg backdrop-blur-sm">
         {session?.user ? (
           <Link
             href="/ad/create"
-            className="text-3xl font-bold text-customAccent transition duration-75 group-hover:scale-[1.2]"
+            className="absolute text-3xl font-bold italic text-customAccent transition duration-200 group-hover:scale-[1.1]"
           >
             POST A FREE AD
           </Link>
         ) : (
           <Link
-            href="/ad/create"
-            className="text-3xl font-bold text-customAccent transition duration-75 group-hover:scale-[1.2]"
+            href="/signin"
+            className="text-3xl font-bold italic text-customAccent transition duration-200 group-hover:scale-[1.1]"
           >
             SIGN IN TO POST A FREE AD
           </Link>
