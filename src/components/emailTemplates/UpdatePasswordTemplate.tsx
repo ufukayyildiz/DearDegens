6c686470d3e0b78fd3c8f507d5c68ca9188f7489
DollaHane <shanehubsch@gmail.com>
2024-06-19 10:27:39 +0200

    Styled email templates

--- src/components/emailTemplates/UpdatePasswordTemplate.tsx
+++ src/components/emailTemplates/UpdatePasswordTemplate.tsx
@@ -8,7 +8,12 @@ import {
   Preview,
   Text,
   Link,
+  Column,
+  Img,
+  Row,
+  Section,
 } from "@react-email/components"
+import { currentYear } from "@/src/lib/utils"
 
 interface EmailTemplateProps {
   userName: string
@@ -20,21 +25,157 @@ export const updatePasswordTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   userName,
   userEmail,
   resetPasswordToken,
-}) => (
-  <Html>
-    <Head />
-    <Preview>DearDegens.com: Update Password</Preview>
-    <Body>
-      <Container>
-        <Heading>Update password for account: {userEmail}</Heading>
-        <Text>Hey {userName}!</Text>
-        <Text>Click on the link below to update your password</Text>
-        <Link
-          href={`${process.env.URL}/reset-password?token=${resetPasswordToken}`}
-        >
-          Click here to reset password
-        </Link>
-      </Container>
-    </Body>
-  </Html>
-)
+}) => {
+  const fontFamily = "HelveticaNeue,Helvetica,Arial,sans-serif"
+
+  const main = {
+    backgroundColor: "#efeef1",
+    fontFamily,
+  }
+
+  const paragraph = {
+    lineHeight: 1.5,
+    fontSize: 14,
+  }
+
+  const container = {
+    maxWidth: "580px",
+    margin: "30px auto",
+    backgroundColor: "#ffffff",
+  }
+
+  const footer = {
+    maxWidth: "580px",
+    margin: "0 auto",
+  }
+
+  const content = {
+    padding: "5px 20px 10px 20px",
+  }
+
+  const logo = {
+    display: "flex",
+    justifyContent: "center",
+    alingItems: "center",
+    padding: 10,
+  }
+
+  const sectionsBorders = {
+    width: "100%",
+    display: "flex",
+  }
+
+  const sectionBorder = {
+    borderBottom: "1px solid rgb(255, 136, 0)",
+    width: "249px",
+  }
+
+  const sectionCenter = {
+    borderBottom: "1px solid rgb(145,71,255)",
+    width: "102px",
+  }
+
+  const link = {
+    textDecoration: "underline",
+  }
+
+  return (
+    <Html>
+      <Head />
+      <Preview>Update your DearDegens password</Preview>
+      <Body style={main}>
+        <Container style={container}>
+          <Section style={logo}>
+            <Img
+              width={500}
+              src={
+                "https://utfs.io/f/b862962d-902a-4cdf-9b5d-8e46e3742cf8-k3gcps.png"
+              }
+            />
+          </Section>
+
+          <Section style={sectionsBorders}>
+            <Row>
+              <Column style={sectionBorder} />
+              <Column style={sectionBorder} />
+              <Column style={sectionBorder} />
+            </Row>
+          </Section>
+
+          <Section style={content}>
+            <Text style={paragraph}>Hi, {userName}</Text>
+            <Text style={paragraph}>
+              You requested to reset your password for your DearDegens account{" "}
+              {userEmail}. If this was not you, please contact us urgently at
+              support@deardegens.com
+            </Text>
+            <Text style={paragraph}>
+              However if you did request this password change, please click on
+              the following link to{" "}
+              <Link
+                href={`${process.env.URL}/reset-password?token=${resetPasswordToken}`}
+                style={link}
+              >
+                reset your account password
+              </Link>{" "}
+              immediately.
+            </Text>
+            <Text style={paragraph}>
+              Remember to use a password that is both strong and unique to your
+              DearDegens account.
+            </Text>
+            <Text style={paragraph}>
+              Still have questions? Please contact{" "}
+              <Link href="mailto:support@deardegens.com" style={link}>
+                DearDegens Support
+              </Link>
+            </Text>
+            <Text style={paragraph}>
+              Thanks,
+              <br />
+              DearDegens Support Team
+            </Text>
+          </Section>
+        </Container>
+
+        <Section style={footer}>
+          {/* <Row>
+      <Column align="right" style={{ width: "50%", paddingRight: "8px" }}>
+        <Img src={`${baseUrl}/static/twitch-icon-twitter.png`} />
+      </Column>
+      <Column align="left" style={{ width: "50%", paddingLeft: "8px" }}>
+        <Img src={`${baseUrl}/static/twitch-icon-facebook.png`} />
+      </Column>
+    </Row> */}
+          <Row>
+            <Text style={{ textAlign: "center", color: "#706a7b" }}>
+              Copyright © {currentYear} DearDegens.com. All rights reserved.
+            </Text>
+            <Text style={{ textAlign: "center", color: "#706a7b" }}>
+              This is a once off notification that contains important
+              information for your attention.
+            </Text>
+            <Text style={{ textAlign: "center", color: "#706a7b" }}>
+              No unsubscribe option available.
+            </Text>
+            <Text style={{ textAlign: "center", color: "#706a7b" }}>
+              <Link
+                href="https://www.deardegens.com/termsofservice"
+                target="_blank"
+              >
+                T's & C's
+              </Link>{" "}
+              |{" "}
+              <Link
+                href="https://www.deardegens.com/privacypolicy"
+                target="_blank"
+              >
+                Privacy
+              </Link>
+            </Text>
+          </Row>
+        </Section>
+      </Body>
+    </Html>
+  )
+}
--- src/components/emailTemplates/listingReportTemplate.tsx
+++ src/components/emailTemplates/listingReportTemplate.tsx
@@ -7,7 +7,13 @@ import {
   Html,
   Preview,
   Text,
+  Link,
+  Column,
+  Img,
+  Row,
+  Section,
 } from "@react-email/components"
+import { currentYear } from "@/src/lib/utils"
 
 interface EmailTemplateProps {
   userName: string
@@ -25,19 +31,144 @@ export const listingReportTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   adId,
   description,
   infraction,
-}) => (
-  <Html>
-    <Head />
-    <Preview>Report sent by: {userName}</Preview>
-    <Body>
-      <Container>
-        <Heading>Listing Report: {reportId}</Heading>
-        <Text>The following report was recieved for listing ID: {adId}</Text>
-        <Text>Username: {userName}</Text>
-        <Text>User email: {userEmail}</Text>
-        <Text>Infraction: {infraction}</Text>
-        <Text>User message: {description}</Text>
-      </Container>
-    </Body>
-  </Html>
-)
+}) => {
+  const fontFamily = "HelveticaNeue,Helvetica,Arial,sans-serif"
+
+  const main = {
+    backgroundColor: "#efeef1",
+    fontFamily,
+  }
+
+  const paragraph = {
+    lineHeight: 1.5,
+    fontSize: 14,
+  }
+
+  const container = {
+    maxWidth: "580px",
+    margin: "30px auto",
+    backgroundColor: "#ffffff",
+  }
+
+  const footer = {
+    maxWidth: "580px",
+    margin: "0 auto",
+  }
+
+  const content = {
+    padding: "5px 20px 10px 20px",
+  }
+
+  const logo = {
+    display: "flex",
+    justifyContent: "center",
+    alingItems: "center",
+    padding: 10,
+  }
+
+  const sectionsBorders = {
+    width: "100%",
+    display: "flex",
+  }
+
+  const sectionBorder = {
+    borderBottom: "1px solid rgb(255, 136, 0)",
+    width: "249px",
+  }
+
+  const sectionCenter = {
+    borderBottom: "1px solid rgb(145,71,255)",
+    width: "102px",
+  }
+
+  const link = {
+    textDecoration: "underline",
+  }
+
+  return (
+    <Html>
+      <Head />
+      <Preview>You forgot the password for your DearDegens account</Preview>
+      <Body style={main}>
+        <Container style={container}>
+          <Section style={logo}>
+            <Img
+              width={500}
+              src={
+                "https://utfs.io/f/b862962d-902a-4cdf-9b5d-8e46e3742cf8-k3gcps.png"
+              }
+            />
+          </Section>
+
+          <Section style={sectionsBorders}>
+            <Row>
+              <Column style={sectionBorder} />
+              <Column style={sectionBorder} />
+              <Column style={sectionBorder} />
+            </Row>
+          </Section>
+
+          <Section style={content}>
+            <Text style={paragraph}>Hi DearDegen Support Team,</Text>
+            <Text style={paragraph}>
+              The following report was sent by {userName} ({userEmail}) who has
+              flagged a listing ({adId}) as possibly infringing on the
+              DearDegens terms of service. The details of the report are as
+              follows:
+            </Text>
+            <Text style={paragraph}>Report ID: {reportId}</Text>
+            <Text style={paragraph}>Listing ID: {adId}</Text>
+            <Text style={paragraph}>Infraction reported: {infraction}</Text>
+            <Text style={paragraph}>
+              Message from {userName}: {description}
+            </Text>
+
+            <Text style={paragraph}>
+              Thanks,
+              <br />
+              DearDegens Support Team
+            </Text>
+          </Section>
+        </Container>
+
+        <Section style={footer}>
+          {/* <Row>
+          <Column align="right" style={{ width: "50%", paddingRight: "8px" }}>
+            <Img src={`${baseUrl}/static/twitch-icon-twitter.png`} />
+          </Column>
+          <Column align="left" style={{ width: "50%", paddingLeft: "8px" }}>
+            <Img src={`${baseUrl}/static/twitch-icon-facebook.png`} />
+          </Column>
+        </Row> */}
+          <Row>
+            <Text style={{ textAlign: "center", color: "#706a7b" }}>
+              Copyright © {currentYear} DearDegens.com. All rights reserved.
+            </Text>
+            <Text style={{ textAlign: "center", color: "#706a7b" }}>
+              This is a once off notification that contains important
+              information for your attention.
+            </Text>
+            <Text style={{ textAlign: "center", color: "#706a7b" }}>
+              No unsubscribe option available.
+            </Text>
+            <Text style={{ textAlign: "center", color: "#706a7b" }}>
+              <Link
+                href="https://www.deardegens.com/termsofservice"
+                target="_blank"
+              >
+                T's & C's
+              </Link>{" "}
+              |{" "}
+              <Link
+                href="https://www.deardegens.com/privacypolicy"
+                target="_blank"
+              >
+                Privacy
+              </Link>
+            </Text>
+          </Row>
+        </Section>
+      </Body>
+    </Html>
+  )
+}
