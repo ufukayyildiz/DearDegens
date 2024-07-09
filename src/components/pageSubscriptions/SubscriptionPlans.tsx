15a4d94cc85149645c2ae7391177e3ae58136b73
DollaHane <shanehubsch@gmail.com>
2024-07-09 09:34:32 +0200

    Added @ts-expect-error Server Component

--- src/components/pageSubscriptions/SubscriptionPlans.tsx
+++ src/components/pageSubscriptions/SubscriptionPlans.tsx
@@ -7,6 +7,7 @@ export default function SubscriptionPlans() {
   return (
     <div className="mt-12 flex flex-wrap justify-center gap-6 space-y-0 overflow-visible p-5 sm:mt-16 lg:mx-auto lg:max-w-4xl xl:mx-0 xl:max-w-none">
       {products.map((product: productType) => (
+        // @ts-expect-error Server Component 
         <SubscriptionPlanCard product={product} />
       ))}
     </div>
